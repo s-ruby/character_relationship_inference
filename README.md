91e495595191d4e91b6c665fe7e2f811f77e3b34
Aaron Liberatore <aliberat@google.com>
2021-04-12 00:19:11 +0000

    Add ability to generate dev/test/train sets

--- README.md
+++ README.md
@@ -1,27 +1,30 @@
 # Character Relationship Inference
 
-This respository is a fork from [SpanBert](https://github.com/gkaramanolakis/SpanBERT) which adapted the SpanBERT scripts from Facebook Research to support relation extraction from general documents beyond the TACRED dataset.
+This repository is the for the COMS 4995 Final Project. The project aims to automatically infer character relationships in fiction novels and output character arc visualizaitons. The project uses a variety of techniques in information extraction including snowball tchniques and [SpanBert](https://github.com/gkaramanolakis/SpanBERT).
 
 
 ## Install Requirements
 
-`pip3 install -U pip setuptools wheel` \
-`pip3 install -U spacy`\
-`python3 -m spacy download en_core_web_lg`\
-`pip3 install -r requirements.txt`
+```
+pip3 install -U pip setuptools wheel
+pip3 install -U spacy
+python3 -m spacy download en_core_web_lg
+pip3 install -r requirements.txt
+```
 
-## To download the fine-tuned SpanBERT model run:
+## [Optionally] Download the fine-tuned SpanBERT model run:
 
 
 `bash download_finetuned.sh`
 
 If the `bash` command results in an error, try `brew install wget`
 
-## To Run
 
-`python3 main.py`
+## Training the custom SpanBert Model:
 
-## To Fine-Tune SpanBert
+1. Create Training Data run `python3 run_data_generator.py` and manually annotate the relationship in each sentence. You can use a keyboard interrupt to stop generating training data at any time (control + c)
+
+2. Fine-Tune the SpanBert
 
 To fine-tune the SpanBert model with custom relations, you can add custom relations the the /data folder. Annotations should be derived from the Stanford NLP package. 
 
@@ -39,3 +42,11 @@ python3 run_train_spanbert.py \
   --max_seq_length 128 \
   --output_dir span_bert_training_output
 ```
+
+## To Run
+
+```
+# Optionally pass custom span bert model
+python3 main.py --model_dir=span_bert_training_output
+```
+
---
+++ run_data_generator.py
@@ -0,0 +1,154 @@
+import spacy
+import json 
+import random
+import argparse
+import numpy as np
+from spacy_help_functions import get_entities, create_entity_pairs
+from spacy.language import Language
+from spacy.tokens import Span
+from processors.utils import read_file
+from processors.nlp_provider import NlpProvider
+
+def write_checkpoint(text: str, file='checkpoint.txt'):
+    f = open(file, 'w')
+    f.write(text)
+    f.close()
+
+def read_checkpoint(file='checkpoint.txt'):
+    try:
+        text = read_file(file)
+        return int(text)
+    except:
+        return 0
+
+def read_raw_example_files():
+    friends = []
+    foes = []
+    neutrals = []
+    with open('data/friends.json') as f:
+        friends = json.load(f)
+    with open('data/foes.json') as f:
+        foes = json.load(f)
+    with open('data/neutrals.json') as f:
+        neutrals = json.load(f)
+
+    return (friends, foes, neutrals)
+
+def write_raw_example_files(friends, foes, neutrals):
+    with open('data/friends.json', 'w') as outfile: 
+        json.dump(friends, outfile)
+
+    with open('data/foes.json', 'w') as outfile: 
+        json.dump(foes, outfile)
+
+    with open('data/neutrals.json', 'w') as outfile: 
+        json.dump(neutrals, outfile)
+
+def main():
+    provider = NlpProvider() 
+    doc = provider.process_file('harrypotter1_preprocessed.txt')
+
+    (friends, foes, neutrals) = read_raw_example_files()
+    
+    checkpoint = read_checkpoint()
+
+    try:
+        for (i, sent) in enumerate(doc.sents):  
+            
+            if checkpoint > i:
+                continue
+
+            write_checkpoint(text=str(i))
+
+            sentence_entity_pairs = create_entity_pairs(sent, ['PERSON'])
+            
+            for ep in sentence_entity_pairs:
+                
+                if ep[1][1] != 'PERSON' or ep[2][1] != 'PERSON':
+                    continue    
+
+                example = {
+                    'id': 'example:' + str(i),
+                    'docid': 'docid:' + str(i),
+                    'relation': 'per:title',
+                    'token': [token.text for token in sent],
+                    'subj_start': ep[1][2][0],
+                    'subj_end': ep[1][2][1],
+                    'obj_start': ep[2][2][0],
+                    'obj_end': ep[2][2][1],
+                    'subj_type': 'PERSON',
+                    'obj_type': 'PERSON',
+                    'stanford_pos': [token.pos_ for token in sent],
+                    'stanford_ner': [token.ent_type_ if token.ent_type_ != '' else '0' for token in sent],
+                    'stanford_head': [token.head.i - sent.start for token in sent],
+                    'stanford_deprel': [token.dep_ for token in sent],
+                }
+
+                print('Sentence')
+                print('========')
+                print(sent)
+                print('========')
+                print(ep[1], ep[2])
+                label = input('\nDoes this indicate:\n\t[0] friend\n\t[1] foe\n\t[2] no relation\n')
+            
+                if label == '0':
+                    example['relation'] = 'per:friend'
+                    friends.append(example)
+                elif label == '1':
+                    example['relation'] = 'per:foe'
+                    foes.append(example)
+                elif label == '2':
+                    example['relation'] = 'no_relation'
+                    neutrals.append(example)
+                
+                print('Current Lengths: \n\tFriends: ' + str(len(friends)) + '\n\tFoes: ' + str(len(foes)) + '\n\tNo Relation: ' + str(len(neutrals)))
+
+        write_raw_example_files(friends, foes, neutrals)
+    except KeyboardInterrupt:
+        write_raw_example_files(friends, foes, neutrals)
+
+def distribute_dataset():
+    (friends, foes, neutrals) = read_raw_example_files()
+
+    random.shuffle(friends)
+    random.shuffle(foes)
+    random.shuffle(neutrals)
+
+    train = []
+    test = []
+    dev = []
+
+    friend_parts = np.array_split(friends, 3)
+    foe_parts = np.array_split(foes, 3)
+    neutral_parts = np.array_split(neutrals, 3)
+
+    train.append(list(friend_parts[0]))
+    train.append(list(foe_parts[0]))
+    train.append(list(neutral_parts[0]))
+
+    test.append(list(friend_parts[1]))
+    test.append(list(foe_parts[1]))
+    test.append(list(neutral_parts[1]))
+
+    dev.append(list(friend_parts[2]))
+    dev.append(list(foe_parts[2]))
+    dev.append(list(neutral_parts[2]))
+
+    with open('data/train.json', 'w') as outfile:
+        json.dump(train, outfile)
+    with open('data/test.json', 'w') as outfile:
+        json.dump(test, outfile)
+    with open('data/dev.json', 'w') as outfile:
+        json.dump(dev, outfile)
+
+    print('Successfully created training, test, and dev sets')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--skip_data_gen", default=False, type=bool, required=False)
+    args = parser.parse_args()
+    
+    if not args.skip_data_gen:
+        main() 
+    distribute_dataset()
+    
